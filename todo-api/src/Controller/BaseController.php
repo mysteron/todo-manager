be87dde4f51e7573bed693f1ad032a4c92c72271
Bartosz Milosierny <bartosz@milosierny.net>
2022-02-27 13:25:51 +0100

    Preparing to use jquery

--- todo-api/src/Controller/BaseController.php
+++ todo-api/src/Controller/BaseController.php
@@ -6,7 +6,7 @@ use App\Entity\TodoNote;
 use App\Repository\CategoryRepository;
 use App\Repository\TodoNoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use PhpParser\Node\Expr\Cast\Array_;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,49 +20,72 @@ class BaseController extends AbstractController
   }
   public string $currentRouteName = '';
 
+  private function getCategories(String $field = 'id', String $order = 'ASC')
+  {
+    return $this->catRepo->findBy(
+      array(),
+      array($field => $order)
+    );
+  }
+
+  private function getTodos(String $field = 'id', String $order = 'ASC')
+  {
+    return $this->noteRepo->findBy(
+      array(),
+      array($field => $order)
+    );
+  }
+
   #[Route('/')]
   public function base(): Response
   {
-    return $this->render('base.html.twig');
+    return $this->redirect('/todo-list');
   }
 
   #[Route('/todo-list', methods: ['GET'])]
   public function todoList(): Response
   {
-    $todos = $this->noteRepo->findBy(
-      array(),
-      array('id' => 'ASC')
-    );
-    return $this->render('todos.html.twig', ['currentRoute' => 'todo-list', 'todos' => $todos]);
+    return $this->render('todos.html.twig', [
+      'currentRoute' => 'todo-list',
+      'todos' => $this->getTodos(),
+      'categories' => $this->getCategories()
+    ]);
   }
 
   #[Route('/todo-list', methods: ['POST'])]
   public function todoListSave(Request $request): Response
   {
-    $todos = $this->noteRepo->findBy(
-      array(),
-      array('id' => 'ASC')
-    );
-    return $this->render('todos.html.twig', ['currentRoute' => 'todo-list', 'todos' => $todos]);
+    $task = new TodoNote();
+    $form = $this->createForm(TaskType::class, $task);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $task = $form->getData();
+    }
+    return $this->render('todos.html.twig', [
+      'currentRoute' => 'todo-list',
+      'todos' => $this->getTodos(),
+      'categories' => $this->getCategories()
+    ]);
   }
 
   #[Route('/todo-list/{id}')]
   public function todoEditOnList(int $id): Response
   {
-    $todos = $this->noteRepo->findBy(
-      array(),
-      array('id' => 'ASC')
-    );
-    return $this->render('todos.html.twig', ['currentRoute' => 'todo-list', 'todos' => $todos, 'toEdit' => $id]);
+    return $this->render('todos.html.twig', [
+      'currentRoute' => 'todo-list',
+      'todos' => $this->getTodos(),
+      'categories' => $this->getCategories(),
+      'toEdit' => $id
+    ]);
   }
 
   #[Route('/categories')]
   public function categories(): Response
   {
-    $categories = $this->catRepo->findBy(
-      array(),
-      array('id' => 'ASC')
-    );
-    return $this->render('category-list.html.twig', ['currentRoute' => 'categories', 'categories' => $categories, 'isEditing' => False]);
+    return $this->render('category-list.html.twig', [
+      'currentRoute' => 'categories',
+      'categories' => $this->getCategories(),
+      'isEditing' => False
+    ]);
   }
 }
